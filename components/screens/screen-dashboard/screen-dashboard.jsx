58b7c986041b499166adb5211add18ea369be61a
duy366110 <duy366110@gmail.com>
2024-03-21 22:36:59 +0700

    #Commit 21.03.2024: Config dashboard.

--- components/screens/screen-dashboard/screen-dashboard.jsx
+++ components/screens/screen-dashboard/screen-dashboard.jsx
@@ -1,22 +1,35 @@
 import { useState } from "react";
-import { View, Text, Image } from "react-native";
-import styles from "./screen-dashboard-style";
+import { View, Image } from "react-native";
+import CommonCardDashboard from "../../common/common-card/common-card-dashboard/common-card-dashboard";
+import styles from "./screen-dashboard.style";
 
 const ScreenDashboard = (props) => {
     const [list, setList] = useState([
-        "https://res.cloudinary.com/ditc3z3gj/image/upload/w_200,h_200,c_scale/v1711017633/lands/dashboard/pexels-oesch-jonathan-10420282_fhhpzh.jpg",
-        "https://res.cloudinary.com/ditc3z3gj/image/upload/w_200,h_200,c_scale/v1711017633/lands/dashboard/pexels-oesch-jonathan-10420282_fhhpzh.jpg",
-        "https://res.cloudinary.com/ditc3z3gj/image/upload/w_200,h_200,c_scale/v1711017633/lands/dashboard/pexels-oesch-jonathan-10420282_fhhpzh.jpg",
-        "https://res.cloudinary.com/ditc3z3gj/image/upload/w_200,h_200,c_scale/v1711017633/lands/dashboard/pexels-oesch-jonathan-10420282_fhhpzh.jpg"
+        {
+            title: 'Nhà đất',
+            thumb: "https://res.cloudinary.com/ditc3z3gj/image/upload/w_200,h_200,c_scale/v1711017633/lands/dashboard/pexels-oesch-jonathan-10420282_fhhpzh.jpg",
+        },
+        {
+            title: 'Căn hộ',
+            thumb: "https://res.cloudinary.com/ditc3z3gj/image/upload/w_200,h_200,c_scale/v1711017633/lands/dashboard/pexels-oesch-jonathan-10420282_fhhpzh.jpg",
+        },
+        {
+            title: 'Mặt bằng',
+            thumb: "https://res.cloudinary.com/ditc3z3gj/image/upload/w_200,h_200,c_scale/v1711017633/lands/dashboard/pexels-oesch-jonathan-10420282_fhhpzh.jpg",
+        },
+        {
+            title: 'Phòng trọ',
+            thumb: "https://res.cloudinary.com/ditc3z3gj/image/upload/w_200,h_200,c_scale/v1711017633/lands/dashboard/pexels-oesch-jonathan-10420282_fhhpzh.jpg",
+        },
     ])
 
     return (
         <View style={[styles.screenDashboardContainer]}>
-            {list.length > 0 && list.map((img) => {
+            {list.length > 0 && list.map((elm, index) => {
                 return (
-                    <View style={[styles.dashboardCard]}>
-                        <Image style={[styles.dashboardCardImage]} source={{uri: img}}/>
-                    </View>
+                    <CommonCardDashboard
+                        title={elm.title}
+                        thumb={elm.thumb}/>
                 )
             })}
         </View>
--- components/screens/screen-dashboard/screen-dashboard.style.jsx
+++ components/screens/screen-dashboard/screen-dashboard.style.jsx
@@ -7,6 +7,7 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'row',
         flexWrap: 'wrap',
+        justifyContent: 'space-between',
         gap: 15,
         height: '100%',
         paddingHorizontal: 15,
@@ -17,11 +18,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'red',
         borderRadius: 5,
         overflow: 'hidden',
-        width: '45%'
-    },
-    dashboardCardImage: {
-        height: 130,
-        width: '100%',
+        width: '47%',
     }
 })
 
