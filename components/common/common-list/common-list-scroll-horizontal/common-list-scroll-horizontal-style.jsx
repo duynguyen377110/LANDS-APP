272e6a6ace86e7f2a450e951c01a26ef5ad3a44c
duy366110 <duy366110@gmail.com>
2024-03-26 14:18:42 +0700

    #Commit 26.03.2024: Config render flat list categories.

---
+++ components/common/common-list/common-list-scroll-horizontal/common-list-scroll-horizontal-style.jsx
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    listScrollComponent: {
+        paddingVertical: 15,
+    },
+    viewCategory: {
+        backgroundColor: '#ffffff',
+        borderRadius: 25,
+        borderColor: '#878787',
+        borderWidth: 1,
+        shadowColor: '#171717',
+        shadowOffset: {width: -2, height: 4},
+        shadowOpacity: 0.2,
+        shadowRadius: 25,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 50,
+        marginRight: 15,
+        padding: 15,
+        textAlign: 'center',
+        width: 150,
+        
+    }
+})
+
+export default styles;
