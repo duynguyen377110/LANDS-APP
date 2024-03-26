6b45fa3e04ba83d11c2d933b479f8bf7be9dfac8
duy366110 <duy366110@gmail.com>
2024-03-26 16:22:29 +0700

    #Config screen user information v - 01.

--- components/screens/screen-information/screen-information.jsx
+++ components/screens/screen-information/screen-information.jsx
@@ -1,4 +1,4 @@
-import { ScrollView, View, Text, Image } from "react-native";
+import { ScrollView, View, Text, Image, TouchableOpacity, BackHandler } from "react-native";
 import CommonHeader from "../../common/common-header/common-header";
 import { commonStyles } from "../../../styles";
 import styles from './screen-information-style';
@@ -7,6 +7,10 @@ const iconUser = require("../../../assets/icons/user-profile-svgrepo-com.png");
 const bannerInfor = 'https://res.cloudinary.com/ditc3z3gj/image/upload/v1711441889/lands/banner-infor_pgl7ks.jpg';
 
 const ScreenInformation = (props) => {
+
+    const onExitHandler = (event) => {
+        BackHandler.exitApp();
+    }
 
     return (
         <ScrollView style={{backgroundColor: '#ffffff'}}>
@@ -24,9 +28,33 @@ const ScreenInformation = (props) => {
                 <View style={[styles.inforWrapper]}>
                     <View style={[styles.inforWrapperContent]}>
                         <View style={[styles.inforUserCommon]}>
-                            <Image  style={[{width: 25, height: 25}]} source={iconUser} />
-                            <Text>Nguyen Van A</Text>
+                            <Image  style={[{width: 50, height: 50}]} source={iconUser} />
+                            <Text style={[styles.inforUserCommonName]}>Nguyen Van A</Text>
                         </View>
+
+                        <View style={[styles.inforUserEssental]}>
+                            <Image  style={[styles.inforUserEssentalIcon]} source={iconUser} />
+                            <Text style={[styles.inforUserEssentalTitle]}>nguyenvana@gmail.com</Text>
+                        </View>
+
+                        <View style={[styles.inforUserEssental]}>
+                            <Image  style={[styles.inforUserEssentalIcon]} source={iconUser} />
+                            <Text style={[styles.inforUserEssentalTitle]}>999 999 9989</Text>
+                        </View>
+
+                        <View style={[styles.inforUserEssental]}>
+                            <Image style={[styles.inforUserEssentalIcon]} source={iconUser} />
+                            <Text style={[styles.inforUserEssentalTitle]}>30 Tan Thanh/ Tan Phu/ TP HCM</Text>
+                        </View>
+
+                        <View style={[styles.inforSignout]}>
+                            <TouchableOpacity
+                                onPress={onExitHandler}
+                                style={styles.btnSignout}>
+                                <Text style={[styles.btnSignoutTitle]}>Đăng xuất</Text>
+                            </TouchableOpacity>
+                        </View>
+
                     </View>
                 </View>
             </View>
