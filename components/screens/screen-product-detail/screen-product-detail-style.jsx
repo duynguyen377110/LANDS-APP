947e6b33f503f86f39ec8a3e9d3dd352ce0c1e3d
duy366110 <duy366110@gmail.com>
2024-03-29 21:15:25 +0700

    #Commit 29.03.2024: Config template product detail.

--- components/screens/screen-product-detail/screen-product-detail-style.jsx
+++ components/screens/screen-product-detail/screen-product-detail-style.jsx
@@ -1,4 +1,5 @@
 import { StyleSheet } from "react-native";
+import { commonStyles } from "../../../styles";
 
 const styles = StyleSheet.create({
     productBanner: {
@@ -8,9 +9,60 @@ const styles = StyleSheet.create({
         width: '100%',
     },
     productOwner: {
+        fontSize: 17,
+        fontWeight: '700',
+        marginBottom: 15,
+        textTransform: 'uppercase',
+    },
+    titleProduct: {
+        fontSize: 14,
+        fontWeight: '700',
+        marginBottom: 5,
+        textDecorationLine: 'underline',
+        textTransform: 'uppercase',
+    },
+    // INFORMATION
+    productInfor: {
+        borderColor: commonStyles.common.borderColor,
+        borderWidth: 1,
+        borderRadius: 5,
+        marginBottom: 20,
+    },
+    productInforRow: {
+        borderColor: commonStyles.common.borderColor,
+        borderBottomWidth: 1,
+        flexDirection: 'row',
+        gap: 5,
+    },
+    productInforTitle: {
+        borderColor: commonStyles.common.borderColor,
+        borderRightWidth: 1,
         fontSize: 15,
         fontWeight: '600',
-        textTransform: 'uppercase',
+        paddingHorizontal: 10,
+        paddingVertical: 20,
+        textTransform: 'capitalize',
+        width: 100,
+        verticalAlign: 'middle',
+    },
+    productInforContent: {
+        flex: 1,
+        flexWrap: 'wrap',
+        fontSize: 15,
+        padding: 10,
+        verticalAlign: 'middle',
+    },
+    // VIEW IMAGE
+    productThumbs: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+        gap: 10,
+        paddingBottom: 15,
+    },
+    productThumbsContent: {
+        height: 100,
+        width: '30%'
     }
 })
 
